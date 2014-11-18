b2278b627c9fb1400064a543c132b5c06b10fe67
Joanne Caroll <joannecaroll@UNIT001.(none)>
2014-11-18 12:26:02 +0800

    Changed menu[0] to playButton, highscoreButton, howtoplayButton, settingsButton, exitButton

--- MathBomb/src/com/example/mathbomb/Activity_MainMenu.java
+++ MathBomb/src/com/example/mathbomb/Activity_MainMenu.java
@@ -10,31 +10,31 @@ import android.widget.Button;
 
 public class Activity_MainMenu extends Activity {
 	public static Date prevEasyDate, prevNormalDate, prevHardDate;
-	public Button[] menu = new Button[5];
+	public Button playButton, highscoreButton, howtoplayButton, settingsButton, exitButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mainmenu);
 
-		menu[0] = (Button)findViewById(R.id.playbutton);
-		menu[0].setOnClickListener(new View.OnClickListener() {
+		playButton = (Button)findViewById(R.id.playbutton);
+		playButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent i = new Intent(Activity_MainMenu.this, Activity_NewGame.class);
 				startActivity(i);
 			}
 		});
-		menu[1] = (Button)findViewById(R.id.highscorebutton);
-		menu[1].setOnClickListener(new View.OnClickListener() {
+		highscoreButton = (Button)findViewById(R.id.highscorebutton);
+		highscoreButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent i = new Intent(Activity_MainMenu.this, Activity_HighScore.class);
 				startActivity(i);
 			}
 		});
-		menu[2]	= (Button)findViewById(R.id.howtoplaybutton);
-		menu[3]	= (Button)findViewById(R.id.settingsbutton);
-		menu[4] = (Button)findViewById(R.id.exitbutton);
-		menu[4].setOnClickListener(new View.OnClickListener() {
+		howtoplayButton	= (Button)findViewById(R.id.howtoplaybutton);
+		settingsButton	= (Button)findViewById(R.id.settingsbutton);
+		exitButton = (Button)findViewById(R.id.exitbutton);
+		exitButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				finish();
