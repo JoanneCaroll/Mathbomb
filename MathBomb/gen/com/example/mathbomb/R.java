4c6c21be12d901c235089b8fe1eb1308dcf36085
Joanne Caroll <joannecaroll@UNIT001.(none)>
2014-11-18 10:37:03 +0800

    Deleted Unnecessary files

--- MathBomb/gen/com/example/mathbomb/R.java
+++ MathBomb/gen/com/example/mathbomb/R.java
@@ -1235,7 +1235,8 @@ containing a value of this type.
         public static final int activity_mainmenu=0x7f03001a;
         public static final int activity_startgame=0x7f03001b;
         public static final int list_single=0x7f03001c;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
+        public static final int main_menu=0x7f03001d;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
