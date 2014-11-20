248b88377ab28199042cffeed515fbc2aacf87b5
JoanneCaroll <joannecaroll@UNIT001.(none)>
2014-11-20 12:42:27 +0800

    Revert "Replaced hardcoded strings using strings.xml"
    This reverts commit 3d44e0fa7a6f1f595d941c85fdffa168365b465e.

--- MathBomb/gen/com/example/mathbomb/R.java
+++ MathBomb/gen/com/example/mathbomb/R.java
@@ -76,7 +76,6 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int defaultvalue=0x7f05000d;
         public static final int highscore=0x7f050004;
         public static final int highscoretitle=0x7f050005;
         public static final int menuexit=0x7f05000c;
@@ -86,7 +85,6 @@ public final class R {
         public static final int menusettings=0x7f05000b;
         public static final int ok=0x7f050006;
         public static final int score=0x7f050002;
-        public static final int seconds=0x7f05000e;
         public static final int setRange=0x7f050007;
         public static final int timeleft=0x7f050003;
     }
