3d44e0fa7a6f1f595d941c85fdffa168365b465e
JoanneCaroll <joannecaroll@UNIT001.(none)>
2014-11-20 09:38:31 +0800

    Replaced hardcoded strings using strings.xml

--- MathBomb/src/com/example/mathbomb/Activity_StartGame.java
+++ MathBomb/src/com/example/mathbomb/Activity_StartGame.java
@@ -1,11 +1,9 @@
-
 package com.example.mathbomb;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -20,16 +18,15 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class Activity_StartGame extends Activity {
-
     private Date mDate;
     private SaveScore mSaveScore;
-    private TextView 
+    private TextView
     txtshowScore, txtshowHighScore, txtshowTimeLeft, txtshowResult,
     txtfirstRandomNumber, txtsecondRandomNumber, txtrandomOperator;
-    private int 
+    private int
     firstRandomInteger, secondRandomInteger, randomOperator,
     score, answer,textColor;
-    private final int 
+    private final int
     categoryEasy=0, categoryNormal=1, categoryHard=2,
     timerGameSpan=30000, timerGameSpeed=1000,
     timerCheckSpan=1000, timerCheckSpeed=500, maxIndexOfRandomResults=3,
@@ -44,38 +41,34 @@ public class Activity_StartGame extends Activity {
         "Easy", "Normal", "Hard",
     };
     private Random random = new Random();
-
     // create reusable listener instead of anonymous types
     private OnClickListener choiceClicker = new OnClickListener() {
         @Override
         public void onClick(View v) {
-//          get view id
+            // get view id
             switch (v.getId()) {
             // set apart views
             case (R.id.choice1):
                 choiceText = (String) btnrandomResult[0].getText();
-                break;
+            break;
             case (R.id.choice2):
                 choiceText = (String) btnrandomResult[1].getText();
-                break;
+            break;
             case (R.id.choice3):
                 choiceText = (String) btnrandomResult[2].getText();
-                break;
+            break;
             case (R.id.choice4):
                 choiceText = (String) btnrandomResult[3].getText();
-                break;
+            break;
             }
             // checkanswer only when a button is clicked
             checkAnswer();
         }
     };
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         setContentView(R.layout.activity_startgame);
-        
         txtfirstRandomNumber = (TextView) findViewById(R.id.integer1);
         txtsecondRandomNumber = (TextView) findViewById(R.id.integer2);
         txtrandomOperator = (TextView) findViewById(R.id.operator);
@@ -85,67 +78,59 @@ public class Activity_StartGame extends Activity {
         txtshowResult.setVisibility(View.INVISIBLE);
         txtshowHighScore = (TextView)findViewById(R.id.showhighscore);
         txtshowTimeLeft = (TextView) findViewById(R.id.showtimeleft);
-        
         int index = R.id.choice1;
-        
         for(int i = 0; i<=maxIndexOfRandomResults; i++){
             btnrandomResult[i] = (Button) findViewById(index);
+            index++;
         }
-        
         Bundle getextra = getIntent().getExtras();
         int choices = getextra.getInt("choice");
-        
         try {
             switch(choices) {
-                case categoryEasy :{
-                    SingleRecord easySingleRecord = new SingleRecord(this, SingleRecord.EASYFILENAME);
-                    easySingleRecord.getDetails();
-                    Record easyrecord = easySingleRecord.highScores().get(0);
-                    newScore = easyrecord.getScore();
-                    txtshowHighScore.setText(newScore);
-                    break;
-                }
-                case categoryNormal :{
-                    SingleRecord normalSingleRecord = new SingleRecord(this, SingleRecord.NORMALFILENAME);
-                    normalSingleRecord.getDetails();
-                    Record normalrecord = normalSingleRecord.highScores().get(0);
-                    newScore = normalrecord.getScore();
-                    txtshowHighScore.setText(newScore);
-                    break;
-                }
-                case categoryHard :{
-                    SingleRecord hardSingleRecord = new SingleRecord(this, SingleRecord.HARDFILENAME);
-                    hardSingleRecord.getDetails();
-                    Record hardrecord =hardSingleRecord.highScores().get(0);
-                    newScore = hardrecord.getScore();
-                    txtshowHighScore.setText(newScore);
-                    break;
-                }
-                default:
-                    return;
+            case categoryEasy :{
+                SingleRecord easySingleRecord = new SingleRecord(this, SingleRecord.EASYFILENAME);
+                easySingleRecord.getDetails();
+                Record easyrecord = easySingleRecord.highScores().get(0);
+                newScore = easyrecord.getScore();
+                txtshowHighScore.setText(newScore);
+                break;
+            }
+            case categoryNormal :{
+                SingleRecord normalSingleRecord = new SingleRecord(this, SingleRecord.NORMALFILENAME);
+                normalSingleRecord.getDetails();
+                Record normalrecord = normalSingleRecord.highScores().get(0);
+                newScore = normalrecord.getScore();
+                txtshowHighScore.setText(newScore);
+                break;
+            }
+            case categoryHard :{
+                SingleRecord hardSingleRecord = new SingleRecord(this, SingleRecord.HARDFILENAME);
+                hardSingleRecord.getDetails();
+                Record hardrecord =hardSingleRecord.highScores().get(0);
+                newScore = hardrecord.getScore();
+                txtshowHighScore.setText(newScore);
+                break;
+            }
+            default:
+                return;
             }
         } catch (Exception e) {
-            txtshowHighScore.setText("0");
+            txtshowHighScore.setText(R.string.defaultvalue);
             e.printStackTrace();
         }
-        
-
         resetGame();
-
         for(int i = 0; i<=maxIndexOfRandomResults; i++){
             btnrandomResult[i].setOnClickListener(choiceClicker);
         }
-
         new CountDownTimer(timerGameSpan, timerGameSpeed) {
             public void onTick(long millisUntilFinished) {
-                txtshowTimeLeft.setText(millisUntilFinished / timerGameSpeed + " sec(s)");
+                txtshowTimeLeft.setText(millisUntilFinished / timerGameSpeed + getString(R.string.seconds));
             }
             public void onFinish() {
                 gameOver();
             }
         }.start();
     }
-
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event)
     {
@@ -155,16 +140,12 @@ public class Activity_StartGame extends Activity {
         }
         return super.onKeyDown(keyCode, event);
     }
-    
     public void generateUniqueChoices(int answer) {
         //clear arraylist
         arrayList.clear();
-
         Bundle getextra = getIntent().getExtras();
         int choices = getextra.getInt("choice");
-
         int categorySize = 0;
-        
         if (choices==categoryEasy) {
             categorySize = easySize;
         } else if (choices==categoryNormal) {
@@ -172,29 +153,24 @@ public class Activity_StartGame extends Activity {
         } else if (choices==categoryHard) {
             categorySize = hardSize;
         }
-        
-        for (int i = 0; i < categorySize; i++)  {
+        for (int i = 0; i < categorySize; i++) {
             arrayList.add(i);
         }
-
         //removes answer in random set if its inside the setasy
         if(arrayList.contains(answer)) {
             arrayList.remove(arrayList.get(answer));
-        }  
-
+        }
         for (int i = 0; i <= maxIndexOfRandomResults; i++) {
             int index = new Random().nextInt(arrayList.size());
             btnrandomResult[i].setText(Integer.toString((Integer) arrayList.get(index)));
             arrayList.remove(index);
         }
     }
-
     private void resetGame() {
         final int a = random.nextInt(maxIndexOfRandomResults);
         randomOperator = random.nextInt(subOperator) + addOperator;
         Bundle getextra = getIntent().getExtras();
         int resetchoice = getextra.getInt("choice");
-        
         if(resetchoice == categoryEasy) {
             generateInput(easyMin,easyMax);
         } else if(resetchoice == categoryNormal) {
@@ -202,16 +178,12 @@ public class Activity_StartGame extends Activity {
         } else if(resetchoice == categoryHard) {
             generateInput(hardMin,hardMax);
         }
-        
         txtfirstRandomNumber.setText(Integer.toString(firstRandomInteger));
         txtsecondRandomNumber.setText(Integer.toString(secondRandomInteger));
-        
         calculateAnswer();
         generateUniqueChoices(answer);
-        
         btnrandomResult[a].setText(Integer.toString(answer));
     }
-
     private void calculateAnswer() {
         if (randomOperator == addOperator) {
             txtrandomOperator.setText("+");
@@ -221,7 +193,6 @@ public class Activity_StartGame extends Activity {
             answer = firstRandomInteger - secondRandomInteger;
         }
     }
-
     private void checkAnswer() {
         if (choiceText == Integer.toString(answer)) {
             score++;
@@ -232,7 +203,6 @@ public class Activity_StartGame extends Activity {
             alertText = "Wrong!";
             textColor = Color.RED;
         }
-        
         new CountDownTimer(timerCheckSpan, timerCheckSpeed) {
             public void onTick(long millisUntilFinished) {
                 txtshowResult.setVisibility(View.VISIBLE);
@@ -243,10 +213,8 @@ public class Activity_StartGame extends Activity {
                 txtshowResult.setVisibility(View.INVISIBLE);
             }
         }.start();
-        
         resetGame();
-    }    
-
+    }
     private void gameOver() {
         try {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -254,15 +222,18 @@ public class Activity_StartGame extends Activity {
             .setMessage("Your score is " + txtshowScore.getText())
             .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int id) {
-                    mDate = new Date();	    			
+                    mDate = new Date();
                     try {
                         Bundle getextra = getIntent().getExtras();
                         int choices = getextra.getInt("choice");
-                        if(choices==categoryEasy) {
+                        if(choices==categoryEasy)
+                        {
                             fileName = SingleRecord.EASYFILENAME;
-                        } else if (choices==categoryNormal) {
+                        } else if (choices==categoryNormal)
+                        {
                             fileName = SingleRecord.NORMALFILENAME;
-                        } else if (choices==categoryHard) {
+                        } else if (choices==categoryHard)
+                        {
                             fileName = SingleRecord.HARDFILENAME;
                         }
                         mSaveScore = new SaveScore(Activity_StartGame.this);
@@ -274,12 +245,11 @@ public class Activity_StartGame extends Activity {
                     i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(i);
                 }
-            }).setCancelable(false).show(); 
+            }).setCancelable(false).show();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
     private void generateInput(int min, int max) {
         firstRandomInteger = random.nextInt(max) + min;
         secondRandomInteger = random.nextInt(max) + min;
