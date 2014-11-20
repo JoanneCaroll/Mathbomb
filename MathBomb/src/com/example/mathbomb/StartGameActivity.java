f421fef404b8ff88a16de40a2b7944705f229200
JoanneCaroll <joannecaroll@UNIT001.(none)>
2014-11-20 10:13:55 +0800

    renamed Activity_HighScore into HighScoreActivity

--- MathBomb/src/com/example/mathbomb/StartGameActivity.java
+++ MathBomb/src/com/example/mathbomb/StartGameActivity.java
@@ -17,7 +17,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Activity_StartGame extends Activity {
+public class StartGameActivity extends Activity {
     private Date mDate;
     private SaveScore mSaveScore;
     private TextView
@@ -238,12 +238,12 @@ public class Activity_StartGame extends Activity {
                         {
                             fileName = SingleRecord.HARDFILENAME;
                         }
-                        mSaveScore = new SaveScore(Activity_StartGame.this);
+                        mSaveScore = new SaveScore(StartGameActivity.this);
                         mSaveScore.saveScore(score, mDate, fileName);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-                    Intent i=new Intent(Activity_StartGame.this, Activity_MainMenu.class);
+                    Intent i=new Intent(StartGameActivity.this, MainMenuActivity.class);
                     i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(i);
                 }
